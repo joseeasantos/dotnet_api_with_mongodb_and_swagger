dc52a3139e47f8cd6203140de7811649a15d4a1d
José E A Santos <jose.e.a.santos@gmail.com>
2020-07-28 22:03:29 -0300

    API .net com MongoDB e Swagger

---
+++ Properties/Options/SwaggerOptions.cs
@@ -0,0 +1,11 @@
+namespace API.Properties.Options
+{
+    public class SwaggerOptions
+    {
+
+    public string JSonRoute {get; set;}
+    public string Description {get; set;}
+    public string UIEndpoint {get; set;}
+    
+    }
+}
